e2e8a467bbb2a1dbcb29c5d227af9eb6b2f3018b
905004003 <905004003@L815WD150057.edu.cbe.ab.ca>
2022-03-15 11:50:54 -0600

    Updated 11:50

--- Chapter5/src/GuessingGame.java
+++ Chapter5/src/GuessingGame.java
@@ -1,9 +1,40 @@
+import java.util.Scanner;
 
-public class GuessingGame {
+/*
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+Program: GuessingGame.java          Last Date of this Revision: March 15 2022
 
+Purpose: Ask the user to guess a number between 1 and 20, they input a number til its correct.
+
+Author: Matthew Pierson, 
+School: CHHS
+Course: Computer Programming 20
+ 
+
+*/
+public class GuessingGame 
+{
+
+	public static void main(String[] args) 
+	{
+		Scanner input = new Scanner(System.in);
+		
+		int rdm = (int) (Math.random()*(20-1)) + 1;
+		
+		System.out.println("Enter a number between 1 and 20: ");
+		int num =input.nextInt();
+
+		while (rdm != num && rdm!= num1) {
+			System.out.print("You are Incorrect");
+			
+			System.out.println("Enter a number between 1 and 20: ");
+			int num1 =input.nextInt();
+			
+			
+			
+		}
+		
+		System.out.println("You are correct!");
 	}
 
 }
