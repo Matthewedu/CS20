f8e1574ecccd4c0132cc76ed31d0397f82f2a350
905004003 <905004003@L815WD220031.edu.cbe.ab.ca>
2022-04-20 10:12:02 -0600

    Updated April 20

--- Chapter7/src/AdderGame.java
+++ Chapter7/src/AdderGame.java
@@ -1,9 +1,12 @@
+import java.util.Scanner;
+
 /*
 
 Program: AdderGame.java          Last Date of this Revision: April 19 2022
 
 Purpose: Create an application where the user guesses the answer to an additon problem, 
-awarding points for guessing it in the least amount of tries
+awarding points for guessing it in the least amount of tries and minusing points if incorrect.
+To end, type 99 to end the game ad find total score.
 
 Author: Matthew Pierson, 
 School: CHHS
@@ -12,8 +15,47 @@ Course: Computer Programming 20
 
 */
 public class AdderGame {
+	
+	 static int score=0;
+	
+	public static void Scoring() {
+		
+		score++;
+		System.out.println(" Score: "+score);
+	}
+	 
+	
+	
     public static void main (String [] args){
+    
+    	int m=0; 
+    
+  	Scanner input = new Scanner(System.in);
+  	
+  	
+    while(m==0) {
     	
+    	int a1= (int) (Math.random()*(20-1)) + 1;
+        int a2= (int) (Math.random()*(20-1)) + 1;
+    	int t=a1+a2;
+    	
+    	System.out.print(a1 +"+"+a2);
+    	System.out.print("=");
+    	int num =input.nextInt();
+    
+    	if(num==t) {
+    System.out.println("You are Correct!");
+    Scoring();
+    
+    	}else if(num==99) {
+    m=1;
+    	
+    	}else {
+    System.out.println("You are Incorrect!");
+    	}
+    
     	
     }
+    System.out.println("Thanks For Playing! Total Score: ");
+   }
 }
