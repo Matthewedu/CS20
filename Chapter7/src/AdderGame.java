c3631e906749d84717fa9cbae5cf2f432b264b4f
905004003 <905004003@L815WD220031.edu.cbe.ab.ca>
2022-04-21 11:06:05 -0600

    Updated April 21

--- Chapter7/src/AdderGame.java
+++ Chapter7/src/AdderGame.java
@@ -6,7 +6,7 @@ Program: AdderGame.java          Last Date of this Revision: April 19 2022
 
 Purpose: Create an application where the user guesses the answer to an additon problem, 
 awarding points for guessing it in the least amount of tries and minusing points if incorrect.
-To end, type 99 to end the game ad find total score.
+To end, type 999 to end the game ad find total score.
 
 Author: Matthew Pierson, 
 School: CHHS
@@ -21,9 +21,12 @@ public class AdderGame {
 	public static void Scoring() {
 		
 		score++;
-		System.out.println(" Score: "+score);
+		System.out.println("Points: "+score);
 	}
-	 
+	 public static void Scoringminus() {
+		 score--;
+		 System.out.println("Points: "+score);
+	 }
 	
 	
     public static void main (String [] args){
@@ -32,12 +35,14 @@ public class AdderGame {
     
   	Scanner input = new Scanner(System.in);
   	
-  	
+  	System.out.println("Welcome to the Adder Game! You must add the 2 random integers to be awarded 1 point.");
+  	System.out.println("If you get it wrong, you will lose one point! Enter 999 to quit playing!");
     while(m==0) {
     	
     	int a1= (int) (Math.random()*(20-1)) + 1;
         int a2= (int) (Math.random()*(20-1)) + 1;
     	int t=a1+a2;
+    	
     	
     	System.out.print(a1 +"+"+a2);
     	System.out.print("=");
@@ -47,15 +52,37 @@ public class AdderGame {
     System.out.println("You are Correct!");
     Scoring();
     
-    	}else if(num==99) {
+    	}else if(num==999) {
     m=1;
     	
     	}else {
-    System.out.println("You are Incorrect!");
+    System.out.println("You are Incorrect! The correct answer is: "+ t);
+    Scoringminus();
     	}
     
     	
     }
-    System.out.println("Thanks For Playing! Total Score: ");
+    System.out.println("Thanks For Playing! Total Points: "+score);
    }
 }
+/* Screen Dump
+
+Welcome to the Adder Game! You must add the 2 random integers to be awarded 1 point.
+If you get it wrong, you will lose one point! Enter 999 to quit playing!
+6+3=9
+You are Correct!
+Points: 1
+14+11=25
+You are Correct!
+Points: 2
+14+14=28
+You are Correct!
+Points: 3
+8+12=5
+You are Incorrect! The correct answer is: 20
+Points: 2
+8+19=999
+Thanks For Playing! Total Points: 2
+
+
+*/
